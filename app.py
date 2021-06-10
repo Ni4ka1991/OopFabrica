d5cd144ab2fcd540c53675afb01a80f40a4f280f
Ni4ka1991 <astahova_nica@mail.ru>
2021-06-10 14:06:43 +0300

    Add file app.py coffee_house.py

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ from coffee_house import *
 brands = [ "Philips", "Bosch", "DeLonghi", "Saeco" ]
     
 ingredients = []
-brands_assortment_coffee = loadInfo( "brands_assortment_coffee" )
-
-
-string = f"{brands_assortment_coffee[ 0 ]['assortment']}"
 replace_symb = ["[", "]", "'", " " ]
-pc = replaceSymbols( replace_symb, string )
 
 
 #def coffeeBarista( ):
@@ -23,91 +18,42 @@ while True:
 
     if( option == 1 ):
         print()
-        opt = printActionList( pc, "Assortment of coffee:")
-        
-        if( opt == 1 ):
-            qua = int(input( "How many ... do you want?" )
 
-#        print( f"<{brands_assortment_coffee[0]['name']}>" )
-#        input( "hit enter ..." )
+        opt = printActionList( list_maker( replace_symb, option - 1 ) , "Assortment of coffee:")
+        
+        if( opt > 0 and opt <= len(selectBrands_Coffee( option - 1 ))):
+            quan = int(input( "How many cup of coffee do you whant? --> " ))
+            print( f"You order {quan} cup of coffe {list_maker(replace_symb, option - 1)[opt - 1]}" )
+
+        
+        else:
+            print("Enter correct value")
     
+    elif( option == 2 ):
+        print()
+        printActionList( replaceSymbols( replace_symb, selectBrands_Coffee( option - 1 )), "Assortment of coffee:")
+    
+    elif( option == 3 ):
+        print()
+        printActionList( replaceSymbols( replace_symb, selectBrands_Coffee( option - 1 )), "Assortment of coffee:")
+        
+    elif( option == 4 ):
+        print()
+        printActionList( replaceSymbols( replace_symb, selectBrands_Coffee( option - 1 )), "Assortment of coffee:")
+
     elif( option == 5 ):
         break
+#        print("bla - bla - bla")
+#        input( "hit ..." )
+    
+    else:
+        raise ValueError
+        print( "Enter one of menu item..." )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-necessary_ingredients_index = [ 1, 2, 3, 4, 5 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ingredients.append(createIngredientQuantity( 1, 2 ))
-ingredients.append(createIngredientQuantity( 2, 20 ))
-ingredients.append(createIngredientQuantity( 3, 22))
-ingredients.append(createIngredientQuantity( 4, 25))
-ingredients.append(createIngredientQuantity( 5, 18))
-
-
-
-#    coffee_1 = CoffeeMachine.makeCoffee( CoffeeMachine(brands[brand_index]), coffee_type, coffee_ingredients )
-#    print(coffee_1)
-
-#system( "clear" )
-#produceCoffee( 0, "Espresso" )
 print( f"\
  {ingredients[0]['name']} - {ingredients[0]['quantity']} {ingredients[0]['unit']};\
  {ingredients[1]['name']} - {ingredients[1]['quantity']} {ingredients[1]['unit']};\
         ")
 print()
-print( kind_of_coffee )
+#print( kind_of_coffee )
--- coffee_house.py
+++ coffee_house.py
@@ -7,6 +7,7 @@ from os import system
 
 ingredients    = loadInfo( "ingredients" )
 kind_of_coffee = loadInfo( "kind_of_coffee" )
+brands_assortment_coffee = loadInfo( "brands_assortment_coffee" )
 
 def createIngredientQuantity( ingredient_index, quantity ):    
     return {
@@ -14,6 +15,15 @@ def createIngredientQuantity( ingredient_index, quantity ):
             "quantity" :  quantity,
             "unit"     : ingredients[ingredient_index]['unit']
             }
+
+
+def selectBrands_Coffee( index ):
+    return f"{brands_assortment_coffee[ index ]['assortment']}"
+
+
+def list_maker( replace_symb, key ):
+        return replaceSymbols( replace_symb, selectBrands_Coffee( key ))
+
 
 def printActionList( items, title = None ):
  
@@ -27,7 +37,7 @@ def printActionList( items, title = None ):
      f"{i + 1:2} {items[i]} "
   )
  print( "#" * 20 )
- print( f" For exit enter {len(items) + 1}")
+ print( f" For exit enter {len(items) + 1}" )
  print( "#" * 20 )
  
  option = int( input( " Select menu item >>> " ))
